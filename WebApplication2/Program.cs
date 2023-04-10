1320170102e75029c0d8f9515e2e4fadfbc90652
kardinodef <kardinodef@gmail.com>
2023-04-10 12:16:35 +0430

    ssl

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,7 +1,30 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Net;
 using WebApplication2;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.ConfigureWebHostDefaults(webBuilder =>
+{
+    webBuilder.ConfigureKestrel(options =>
+    {
+        var port = 5001;
+        var pfxFilePath = @"D:\workshop.ursatile.com\certificate.pfx";
+        // The password you specified when exporting the PFX file using OpenSSL.
+        // This would normally be stored in configuration or an environment variable;
+        // I've hard-coded it here just to make it easier to see what's going on.
+        var pfxPassword = "green cairo angle piano";
+
+        options.Listen(IPAddress.Any, port, listenOptions =>
+        {
+            // Enable support for HTTP1 and HTTP2 (required if you want to host gRPC endpoints)
+            listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+            // Configure Kestrel to use a certificate from a local .PFX file for hosting HTTPS
+            listenOptions.UseHttps(pfxFilePath, pfxPassword);
+        });
+    });
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
